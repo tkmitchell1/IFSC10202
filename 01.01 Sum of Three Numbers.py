67cd6cdcfe68843f36f553fbd15e1adad3a23bd2
IceTLemonade <tkmitchell1@ualr.edu>
2025-08-25 15:50:49 +0000

    update

---
+++ 01.01 Sum of Three Numbers.py
@@ -0,0 +1,10 @@
+# Prompt the user for input
+first_number = int(input("First Number: "))
+second_number = int(input("Second Number: "))
+third_number = int(input("Third Number: "))
+
+# Calculate the sum
+total = first_number + second_number + third_number
+
+# Print the result
+print("The sum is:", total)
---
+++ 1.04 Apple Sharing.py
@@ -0,0 +1,6 @@
+n = int(input("Number of Students: "))
+k = int(input("Number of Apples: "))
+applesperstudent = k // n
+remainingapples = k % n
+print (applesperstudent)
+print (remainingapples)
